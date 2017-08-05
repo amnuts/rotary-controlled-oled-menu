e6e8e902c3f0118480db9d45a787b69b451f866b
Andrew Collington <andy@amnuts.com>
2017-08-05 17:48:29 +0100

    Started to determine the range of options to be shown and highlighting them.

--- Menus/Menu.py
+++ Menus/Menu.py
@@ -9,7 +9,7 @@ class Menu:
 
     def __init__(self, options):
         self.options = options
-        self.onOption = -1
+        self.onOption = None
 
         self.oled = SSD1306.SSD1306_128_32(rst=None, gpio=GPIO)
         self.oled.begin()
@@ -24,17 +24,26 @@ class Menu:
         self.draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
 
     def render(self, highlight=None):
+        if highlight < 0:
+            self.onOption = len(self.options) - 1
+        elif highlight > len(self.options) - 1:
+            self.onOption = 0
+        else:
+            self.onOption = highlight
         self.blank()
-        self.build(highlight)
+        self.build()
         self.oled.image(self.image)
         self.oled.display()
 
-    def build(self, highlight=None):
+    def build(self):
+        start = 0
+        if self.onOption > 2:
+            start = self.onOption - 2
         top = -1
-        for x in range(len(self.options)):
+        for x in range(start, start + 3):
             fill = 1
-            if highlight == x:
-                at_row_height = 12 * highlight
+            if self.onOption == x:
+                at_row_height = 12 * (self.onOption % 3)
                 self.draw.rectangle([0, at_row_height - 1, self.oled.width, at_row_height + 12], outline=0, fill=1)
                 fill = 0
             self.draw.text((3, top + 2), self.options[x], font=self.font, fill=fill)
